0b8e8ff84ef200b17f9cdb4780a7a9f44350c947
KimYoungHwan8750 <dnfwlq123@naver.com>
2025-02-25 01:07:13 +0900

    Import EditorModule, CanvasModule

--- src/websocket/websocket.module.ts
+++ src/websocket/websocket.module.ts
@@ -1,7 +1,11 @@
 import { Module } from "@nestjs/common";
 import { WebsocketGateway } from "./websocket.gateway";
+import { EditorService } from "src/editor/editor.service";
+import { EditorModule } from "src/editor/editor.module";
+import { CanvasModule } from "src/canvas/canvas.module";
 
 @Module({
+  imports: [EditorModule, CanvasModule],
   providers: [WebsocketGateway],
 })
 export class WebsocketModule {}
