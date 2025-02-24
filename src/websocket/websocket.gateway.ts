f9368f33970a2e36da76ef151fa8d4411dc29790
KimYoungHwan8750 <dnfwlq123@naver.com>
2025-02-25 01:06:51 +0900

    Websocket Gateway에 EditorService, CanvasService DI 및 Payload 바인딩

--- src/websocket/websocket.gateway.ts
+++ src/websocket/websocket.gateway.ts
@@ -1,21 +1,43 @@
-import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
+import { RouterModule } from "@nestjs/core";
+import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { AppConstant, CanvasData, ChangeLanguagePayload, EditorDatasType, InputTextPayload, SearchPayload } from "shared-coding-gather";
 import { Server, Socket } from "socket.io";
+import { CanvasService } from "src/canvas/canvas.service";
+import { EditorService } from "src/editor/editor.service";
 @WebSocketGateway(80, { namespace: "ws", cors: "localhost:5173"})
 export class WebsocketGateway{
+  constructor(
+    private readonly editorService: EditorService,
+    private readonly canvasService: CanvasService
+  ) {}
+  @WebSocketServer()
   server: Server
+  canvasData: CanvasData
 
   @SubscribeMessage("inputText")
-  inputText(@ConnectedSocket() socket: Socket, @MessageBody() data: string): void {
-    socket.broadcast.emit("inputText", data);
+  inputText(@ConnectedSocket() socket: Socket, @MessageBody() payload: InputTextPayload): void {
+    if(payload.direction === AppConstant.direction.TOP) {
+      this.editorService.editorData.topEditorData.setText(payload.text);
+    } else {
+      this.editorService.editorData.bottomEditorData.setText(payload.text);
+    }
+    socket.broadcast.emit("inputText", payload);
+    console.log(this.editorService.editorData.bottomEditorData.toString());
   }
 
   @SubscribeMessage("changeLanguage")
-  changeLanguage(@ConnectedSocket() socket: Socket, @MessageBody() data: string): void {
-    socket.broadcast.emit("changeLanguage", data);
+  changeLanguage(@ConnectedSocket() socket: Socket, @MessageBody() payload: ChangeLanguagePayload): void {
+    if(payload.direction === AppConstant.direction.TOP) {
+      this.editorService.editorData.topEditorData.setLanguage(payload.language);
+    } else {
+      this.editorService.editorData.bottomEditorData.setLanguage(payload.language);
+    }
+    socket.broadcast.emit("changeLanguage", payload);
   }
 
   @SubscribeMessage("search")
-  search(@ConnectedSocket() socket: Socket, @MessageBody() data: string): void {
-    // socket.broadcast.emit("search", data);
+  search(@ConnectedSocket() socket: Socket, @MessageBody() payload: SearchPayload): void {
+    this.canvasService.setUrl(payload.url);
+    console.log(this.canvasService.toString());
   }
 }
