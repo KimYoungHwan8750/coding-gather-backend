6d3f500b1e82050b4ff6bce852a6663fe8806000
KimYoungHwan8750 <dnfwlq123@naver.com>
2025-02-25 03:30:38 +0900

    WebsocketService 추가

--- src/websocket/websocket.module.ts
+++ src/websocket/websocket.module.ts
@@ -3,9 +3,11 @@ import { WebsocketGateway } from "./websocket.gateway";
 import { EditorService } from "src/editor/editor.service";
 import { EditorModule } from "src/editor/editor.module";
 import { CanvasModule } from "src/canvas/canvas.module";
+import { WebsocketService } from "./websocket.service";
+import { WebsocketInterceptor } from "./websocket.interceptor";
 
 @Module({
   imports: [EditorModule, CanvasModule],
-  providers: [WebsocketGateway],
+  providers: [WebsocketGateway, WebsocketService, WebsocketInterceptor],
 })
 export class WebsocketModule {}
