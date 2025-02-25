cce1bef94fd064308f7a6b9319d65920ed73e048
KimYoungHwan8750 <dnfwlq123@naver.com>
2025-02-26 03:01:15 +0900

    크롤링 모듈 작성, 필요없는 element 삭제 후 스크린샷

--- src/crawling/crawling.service.ts
+++ src/crawling/crawling.service.ts
@@ -1,8 +1,41 @@
-import { WebSocketGateway } from "@nestjs/websockets";
+import { Injectable, OnApplicationShutdown, OnModuleInit } from "@nestjs/common";
+import { Browser, chromium, Page } from "playwright";
 
-@WebSocketGateway(80, { namespace: 'events' })
-export class CrawlingService {
-  constructor() {
-    console.log('CrawlingService constructor');
+@Injectable()
+export class CrawlingService implements OnModuleInit, OnApplicationShutdown{
+  private browser: Browser;
+  private page: Page;
+
+  async onModuleInit() {
+    this.browser = await chromium.launch();
+    this.page = await this.browser.newPage({userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"});
   }
+
+  async onApplicationShutdown() {
+    await this.browser.close();
+  }
+
+  async getBrowser() {
+    return this.browser;
+  }
+
+  async getScreenshot(url: string): Promise<Buffer<ArrayBufferLike>> {
+    try {
+      await this.page.goto(url);
+    /**
+     * 바디를 지우고 row 클래스를 가진 div요소만 남깁니다.
+     * 이렇게 하면 현재는 본문만 남는 정상적인 동작이지만
+     * 사이트 구조가 바뀌면 제대로 동작하지 않을 수 있습니다.
+     */
+      await this.page.evaluate(() => {
+        const row = document.querySelector("div[class='row']");
+        document.body.innerHTML = "";
+        document.body.appendChild(row);
+      })
+    } catch (err) {
+      throw new Error("스크린샷을 찍을 수 없습니다.");
+    }
+    return await this.page.screenshot({path: "screenshot.png", fullPage: true, type: "png"});
+  }
+
 }
