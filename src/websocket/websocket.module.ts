10158675c6c70a0913c67719fddcbe8c05333e31
KimYoungHwan8750 <dnfwlq123@naver.com>
2025-02-26 03:02:43 +0900

    삼항연산자 사용으로 가독성 개선, 크롤링 메서드 성공 실패 분기처리 및 웹소켓 모듈 작성

--- src/websocket/websocket.module.ts
+++ src/websocket/websocket.module.ts
@@ -1,13 +1,13 @@
 import { Module } from "@nestjs/common";
 import { WebsocketGateway } from "./websocket.gateway";
-import { EditorService } from "src/editor/editor.service";
 import { EditorModule } from "src/editor/editor.module";
 import { CanvasModule } from "src/canvas/canvas.module";
 import { WebsocketService } from "./websocket.service";
 import { WebsocketInterceptor } from "./websocket.interceptor";
+import { CrawlingModule } from "src/crawling/crawling.module";
 
 @Module({
-  imports: [EditorModule, CanvasModule],
+  imports: [EditorModule, CanvasModule, CrawlingModule],
   providers: [WebsocketGateway, WebsocketService, WebsocketInterceptor],
 })
 export class WebsocketModule {}
--- src/websocket/websocket.service.ts
+++ src/websocket/websocket.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from "@nestjs/common";
-import { AppConstant, ChangeLanguagePayload, ChangeLanguageResponse, InputTextPayload, InputTextResponse, SearchPayload } from "shared-coding-gather";
+import { AppConstant, ChangeLanguagePayload, ChangeLanguageResponse, InputTextPayload, InputTextResponse, SearchPayload, SearchResponse } from "shared-coding-gather";
 import { CanvasService } from "src/canvas/canvas.service";
 import { EditorService } from "src/editor/editor.service";
 import { Socket } from "socket.io";
+import { CrawlingService } from "src/crawling/crawling.service";
 
 @Injectable()
 export class WebsocketService {
@@ -10,7 +11,8 @@ export class WebsocketService {
 
   constructor(
     private readonly editorService: EditorService,
-    private readonly canvasService: CanvasService
+    private readonly canvasService: CanvasService,
+    private readonly crawlingService: CrawlingService
   ) {}
 
 
@@ -18,11 +20,8 @@ export class WebsocketService {
     this.socket = socket;
   }
   receiveInputTextEvent(payload: InputTextPayload) {
-    if(payload.direction === AppConstant.direction.TOP) {
-      this.editorService.getTopEditorData().setText(payload.text);
-    } else {
-      this.editorService.getBottomEditorData().setText(payload.text);
-    }
+    const editor = payload.direction === AppConstant.direction.TOP ? this.editorService.getTopEditorData() : this.editorService.getBottomEditorData();
+    editor.setText(payload.text);
   }
 
   transmitInputTextEvent(payload: InputTextPayload) {
@@ -31,15 +30,12 @@ export class WebsocketService {
       text: this.editorService.getText(payload.direction),
       language: this.editorService.getLanguage(payload.direction)
     }
-    this.socket.emit(AppConstant.websocketEvent.INPUT_TEXT, data);
+    this.socket.broadcast.emit(AppConstant.websocketEvent.INPUT_TEXT, data);
   }
 
   receiveChangeLanguageEvent(payload: ChangeLanguagePayload) {
-    if(payload.direction === AppConstant.direction.TOP) {
-      this.editorService.getTopEditorData().setLanguage(payload.language);
-    } else {
-      this.editorService.getBottomEditorData().setLanguage(payload.language);
-    }
+    const editor = payload.direction === AppConstant.direction.TOP ? this.editorService.getTopEditorData() : this.editorService.getBottomEditorData();
+    editor.setLanguage(payload.language);
   }
 
   transmitChangeLanguageEvent(payload: ChangeLanguagePayload) {
@@ -50,11 +46,26 @@ export class WebsocketService {
     this.socket.emit(AppConstant.websocketEvent.CHANGE_LANGUAGE, data);
   }
 
-  receiveSearchEvent(payload: SearchPayload) {
+  async receiveSearchEvent(payload: SearchPayload) {
+    const binary = await this.crawlingService.getScreenshot(payload.url);
     this.canvasService.setUrl(payload.url);
+    this.canvasService.setBinary(binary);
   }
 
   transmitSearchEvent() {
+    let data: SearchResponse;
+    const emit = () => this.socket.emit(AppConstant.websocketEvent.SEARCH, data);
+    const ok = () => {
+      data.code = 200;
+      data.binary = this.canvasService.getBinary();
+      emit();
+    }
+    const fail = () => {
+      data.code = 404;
+      data.binary = null;
+      emit();
+    }
+    return { ok, fail }
   }
 
 }
